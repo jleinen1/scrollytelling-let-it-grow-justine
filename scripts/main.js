7529aa14fb057953a11434a96f300bf9f00ea69e
Justine Leinen <jleinen1@ewu.edu>
2026-03-07 00:54:55 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
+
+// ch 1-seed spill interaction
+
+const packet = document.querySelector(".seed-packet");
+const seeds = document.querySelector(".seeds-scatter");
+
+const seedTimeline = gsap.timeline({ paused: true });
+
+seedTimeline
+  .to(packet, {
+    rotation: 40,
+    duration: 0.6,
+    ease: "power2.out"
+  })
+  .fromTo(seeds,
+    {
+      y: -20,
+      opacity: 0
+    },
+    {
+      y: 80,
+      opacity: 1,
+      duration: 0.8,
+      ease: "power2.out"
+    },
+    "-=0.2"
+  );
+
+packet.addEventListener("click", () => {
+  seedTimeline.play();
+});
+
 
